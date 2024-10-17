ffc3efd46f4bb32627a8cd51ac99970274b46b35
Danilo <damilhome@gmail.com>
2024-10-17 15:20:27 -0300

    controlador carrossel e animacao cards

---
+++ assets/scripts/card-hover-animation.js
@@ -0,0 +1,18 @@
+function handleMouseEnter() {
+    this.classList.add('s-card--hovered');
+}
+
+function handleMouseLeave() {
+    this.classList.remove('s-card--hovered');
+}
+
+function addEventListenersToCards() {
+    const cardElements = document.getElementsByClassName('s-card');
+    
+    [...cardElements].forEach(card => {
+        card.addEventListener('mouseenter', handleMouseEnter)
+        card.addEventListener('mouseleave', handleMouseLeave)
+    });
+}
+
+document.addEventListener('DOMContentLoaded', addEventListenersToCards, false);
